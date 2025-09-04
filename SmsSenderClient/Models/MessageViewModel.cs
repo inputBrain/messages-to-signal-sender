0dd6521de2eb6d9586d1063a90f16a777d3e5ba2
Oleksandr Voznyi <vsanyinclude@gmail.com>
2025-09-04 13:27:45 +0300

    fix: centralization form in the middle on the screen

--- SmsSenderClient/Models/MessageViewModel.cs
+++ SmsSenderClient/Models/MessageViewModel.cs
@@ -6,7 +6,7 @@ public class MessageViewModel
 {
     public string? Name { get; set; }
     
-    [MaxLength(500)]
-    [Required(ErrorMessage = " error")]
+    [MaxLength(500, ErrorMessage = "The message cannot exceed 500 characters.")]
+    [Required(ErrorMessage = "The message field is required.")]
     public string Message { get; set; }
 }
