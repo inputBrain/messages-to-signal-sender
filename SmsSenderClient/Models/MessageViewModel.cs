6c76c68f7ab602c8e7a90fe8e52af87ae3869688
Oleksandr Voznyi <vsanyinclude@gmail.com>
2025-09-19 11:27:14 +0300

    fix: Username cannot be null

--- SmsSenderClient/Models/MessageViewModel.cs
+++ SmsSenderClient/Models/MessageViewModel.cs
@@ -7,7 +7,7 @@ public class MessageViewModel
     [Required(ErrorMessage = "Обов'язкове поле")]
     [MinLength(2, ErrorMessage = "Ім'я не може бути меншим ніж 2 символи.")]
     [MaxLength(50, ErrorMessage = "Ім'я не може перевищувати 50 символів.")]
-    public string? Name { get; set; }
+    public string Name { get; set; }
     
     [MaxLength(500, ErrorMessage = "Повідомлення не може перевищувати 500 символів.")]
     [Required(ErrorMessage = "Обов'язкове поле")]
